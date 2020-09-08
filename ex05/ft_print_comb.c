116e5a4bb13acf505ffe8128ebbcf18198c01e71
ashish0248 <61741711+ashish0248@users.noreply.github.com>
2020-09-08 22:07:18 +0900

    add ft_print_comb

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -66,3 +66,31 @@ void	ft_print_comb()
 		c = b + 1;
 	}
 }
+
+void	ft_print_comb()
+{
+	char a;
+	char b;
+	char c;
+	char d;
+	char e;
+
+	ft_print_comb01(&a, &b, &c, &d, &e);
+
+	while (a <= 55)
+	{
+		while (b <= 56)
+		{
+			while (c <= 57)
+			{
+				ft_print_comb02(a, b, c, d, e);
+				c++;
+			}
+			b++;
+			c = b +1;
+		}
+		a++;
+		b = a + 1;
+		c = b + 1;
+	}
+}
