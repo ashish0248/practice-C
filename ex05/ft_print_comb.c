a8429feb30b5eb6e46117b727fa98196269593a8
ashish0248 <61741711+ashish0248@users.noreply.github.com>
2020-09-23 23:13:59 +0900

    Update ft_print_comb.c

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -63,22 +63,4 @@ void	ft_print_comb()
 	char d;
 	char e;
 
-	ft_print_comb01(&a, &b, &c, &d, &e);
-
-	while (a <= 55)
-	{
-		while (b <= 56)
 		{
-			while (c <= 57)
-			{
-				ft_print_comb02(a, b, c, d, e);
-				c++;
-			}
-			b++;
-			c = b +1;
-		}
-		a++;
-		b = a + 1;
-		c = b + 1;
-	}
-}
