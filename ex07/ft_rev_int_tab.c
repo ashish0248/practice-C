d0382e0633bdcac39cf4f31869810b22686c7cc3
ashish0248 <61741711+ashish0248@users.noreply.github.com>
2020-09-24 23:37:41 +0900

    adjust file

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -19,3 +19,25 @@ void	ft_rev_int_tab(int *tab, int size)
 		size--;
 	}
 }
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int number[size];
+	int *q;
+	int i = 0;
+
+	i = 0;
+	q = &number[0];
+	while(tab[size] != '\0')
+	{
+		number[size] = tab[size];
+		size++;
+	}
+	number[size] = '\n';
+	while(size == 0)
+	{
+		tab[i] = number[size - 1];
+		i++;
+		size--;
+	}
+}
