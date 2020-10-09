5a1da819291d0ad2870e731e77a1abe4c76315b8
ashish0248 <61741711+ashish0248@users.noreply.github.com>
2020-10-09 22:14:57 +0900

    adjust file

--- ex07/main.c
+++ ex07/main.c
@@ -31,19 +31,3 @@ int main(void)
 	ft_rev_int_tab(tab, size);
 	return (0);
 }
-
-int main(void)
-{
-	int str [4];
-	int *tab;
-	int size;
-
-	str[0] = 3;
-	str[1] = 2;
-	str[2] = 1;
-	str[3] = 0;
-	size = 4;
-	tab = &str[0];
-	ft_rev_int_tab(tab, size);
-	return (0);
-}
