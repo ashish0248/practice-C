b616a1d8377e01b93f1dac05054276b363df7889
ashish0248 <61741711+ashish0248@users.noreply.github.com>
2021-02-13 22:44:42 +0900

    Update ft_sort_int_tab.c

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -20,5 +20,24 @@ void	ft_sort_int_tab(int *tab, int size)
 	}
 }
 
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int n;
+	int c;
 
-
+	n = 10;
+	while(n + 1 == size)
+	{
+		if (tab[n] < tab[n + 1])
+		{
+			n++;
+		}
+		else
+		{
+			c = tab[n + 1];
+			tab[n + 1] = tab[n];
+			tab[n] = c;
+			n = 0;
+		}
+	}
+}
